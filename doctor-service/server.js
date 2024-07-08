4f48ff31b4362a8a0c22baff7f6540ca6317ef1d
jdhidal <87213643+jdhidal@users.noreply.github.com>
2024-07-08 17:52:28 -0500

    Update server.js

--- doctor-service/server.js
+++ doctor-service/server.js
@@ -5,7 +5,8 @@ const port = 5001;
 const doctors = [
   { id: 1, name: 'Dr. Smith', specialty: 'Cardiology' },
   { id: 2, name: 'Dr. Jones', specialty: 'Neurology' },
-  { id: 3, name: 'Dr. Kevin', specialty: 'Ginecology' }
+  { id: 3, name: 'Dr. Kevin', specialty: 'Ginecology' },
+  { id: 4, name: 'Dr. Ariel', specialty: 'CR7' }
 ];
 
 app.get('/doctors', (req, res) => {
